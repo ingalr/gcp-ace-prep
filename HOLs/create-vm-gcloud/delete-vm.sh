bef7a2b61dfb7468ff2f350b56836087e3c60e73
ingalr <al.rodriguezc01@hotmail.com>
2025-02-19 18:16:32 -0500

    Update delete-vm.sh

--- HOLs/create-vm-gcloud/delete-vm.sh
+++ HOLs/create-vm-gcloud/delete-vm.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-PROJECT_ID="your-project-id" #The same you used in Step 1
-ZONE="us-central1-a" #The same you used in Step 1
-VM_NAME="my-first-gcloud-vm" #The same you used in Step 1
+#The same you used in Step 1
+PROJECT_ID="your-project-id" 
+ZONE="us-central1-a"
+VM_NAME="my-first-gcloud-vm"
 
-echo "🛑 Deleting VM instance..."
+echo "Deleting VM instance..."
 gcloud compute instances delete $VM_NAME \
     --project=$PROJECT_ID \
     --zone=$ZONE \
     --quiet
 
-echo "✅ VM $VM_NAME deleted!"
+echo "VM $VM_NAME deleted!"
